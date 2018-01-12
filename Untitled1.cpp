07cb4047d3f20f8b1a8a1a3e8c640db262d0784f
jnpat <35354784+jnpat@users.noreply.github.com>
2018-01-12 16:42:08 +0700

    week2

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	float sum=0,x=2;
+	while(x<=69){
+		sum = sum + (1/x);
+		x = x+1;
+		
+	}
+	cout << sum;
+	return 0;
+}
+	
